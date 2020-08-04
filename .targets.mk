89ac0e932f22e4e5ce3b80b42cfb88ed6e26f111
Adam Montville <adam.w.montville@gmail.com>
2020-08-04 16:41:20 -0500

    Fixed build
    capability-advertisement-handshake didn't have a link, so I created one (see "## TEST" section in the markdown file. Also, put line feeds in Periodic, Observational/Event-based and the like).

--- .targets.mk
+++ .targets.mk
@@ -1,4 +1,4 @@
 TARGETS_DRAFTS := draft-ietf-sacm-arch
 TARGETS_TAGS := 
-draft-ietf-sacm-arch-00.xml: draft-ietf-sacm-arch.xml
-	sed -e 's/draft-ietf-sacm-arch-latest/draft-ietf-sacm-arch-00/g' -e 's/draft-ietf-sacm-arch-latest/draft-ietf-sacm-arch-00/g' -e 's/draft-ietf-sacm-arch-latest/draft-ietf-sacm-arch-00/g' -e 's/draft-ietf-sacm-arch-latest/draft-ietf-sacm-arch-00/g' $< >$@
+draft-ietf-sacm-arch-00.txt: draft-ietf-sacm-arch.txt
+	sed -e 's/draft-ietf-sacm-arch-latest/draft-ietf-sacm-arch-00/g' -e 's/draft-ietf-sacm-arch-latest/draft-ietf-sacm-arch-00/g' -e 's/draft-ietf-sacm-arch-latest/draft-ietf-sacm-arch-00/g' $< >$@
